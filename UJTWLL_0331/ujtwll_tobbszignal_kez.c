e33e541a2c5c0cdf77de80ed2a3ad49c12995e63
UJTWLL <79086854+UJTWLL@users.noreply.github.com>
2021-05-14 20:35:02 +0200

    Add files via upload

--- UJTWLL_0331/ujtwll_tobbszignal_kez.c
+++ UJTWLL_0331/ujtwll_tobbszignal_kez.c
@@ -2,27 +2,52 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void handle_sigint_elso(int sig, int szamlalo){
-    printf("Felfogott jel: SIGINT(%d)\n", sig);
-    szamlalo++;
-}
-void sig_dfl(int sig){
-    printf("Felfogott jel: SIGINT(%d)\n", sig);
-}
-void handle_sigquit(int sig){
-    printf("Felfogott jel: SIGQUIT(%d)\n", sig);
-}
-void handle_sigdfl(int sig){
-    printf("Felfogott jel: SIGDFL(%d)\n", sig);
-}
+void handleSignals(int signal);
 
-int main() {
-    int szamlalo = 0;
-    signal(SIGINT, szamlalo>1?handle_sigint_elso:sig_dfl);
-    signal(SIGQUIT, handle_sigquit);
-    while(1){
-        printf("Varakozas\n");
-        sleep(1);
-    }
-    return 0;
+int main()
+{
+	void (*sigHandlerInterrupt)(int);
+	void (*sigHandlerQuit)(int);
+	void (*sigHandlerReturn)(int);
+
+	sigHandlerInterrupt = sigHandlerQuit = handleSignals;
+	sigHandlerReturn = signal(SIGINT, sigHandlerInterrupt);
+
+	if (sigHandlerReturn == SIG_ERR)
+	{
+		perror("signal error: ");
+		return 1;
+	}
+
+	sigHandlerReturn = signal(SIGQUIT, sigHandlerQuit);
+
+	if (sigHandlerReturn == SIG_ERR)
+	{
+		perror("signal error: ");
+		return 1;
+	}
+	while (1)
+	{
+		printf("A program leallitasahoz:\n");
+		printf("\tNyisson meg egy masik terminalt\n");
+		printf("\tAdja ki a parancsot: kill %d vagy Control + C ketszer\n", getpid());
+		sleep(10);
+	}
+	return 0;
+}
+void handleSignals(int signalNumber)
+{
+	switch (signalNumber)
+	{
+	case SIGINT:
+		printf("Nyomja meg a Control + C billentyukombinaciot!\n");
+		signal(SIGINT, SIG_DFL);
+		break;
+	case SIGQUIT:
+		printf("Nyomja meg a Control +\\\n");
+		break;
+	default:
+		printf("Fogadott signal szama: %d\n", signalNumber);
+		break;
+	}
 }
